2420f9927e31d3f00dfc588d1231acce2d54c8a9
prajwal <prajwalthete98@gmail.com>
2023-12-28 23:31:43 +0530

    first controller and all setup config

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-dotenv.config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({ path: "./env" });
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 3000, () => {
+    app.listen(process.env.PORT || 8000, () => {
       console.log(`server running on port ${process.env.PORT}`);
     });
   })
   .catch((err) => {
     console.log(`mongoDB connection Failed !!`, err);
   });
-
-
-
-
-
 
 /* 
 
